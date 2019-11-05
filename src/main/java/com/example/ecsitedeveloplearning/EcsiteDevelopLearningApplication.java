fb24748de90640757fc2d95eadec464c3fcce63d
石山拓巳 <@DESKTOP-9CL1P8P>
2019-11-06 00:46:45 +0900

    2019/11/05 Thymeleaf設定追記

--- src/main/java/com/example/ecsitedeveloplearning/EcsiteDevelopLearningApplication.java
+++ src/main/java/com/example/ecsitedeveloplearning/EcsiteDevelopLearningApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcsiteDevelopLearningApplication {
 
 	public static void main(String[] args) {
+		/**
+		デフォルトだとHTML5モードで起動する。
+		Thymeleaf ver 3.x 系ではHTMLモードでの起動が推奨される。
+		これを記述しておくことで、HTMLモードで起動する
+		*/
 		SpringApplication.run(EcsiteDevelopLearningApplication.class, args);
 	}
 }
