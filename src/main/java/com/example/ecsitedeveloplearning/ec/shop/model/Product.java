49329dfda963da4bc033515c063b896fb088e0bb
石山拓巳 <@DESKTOP-9CL1P8P>
2019-12-05 07:29:38 +0900

    TopPage作成 ShopPage用Model定義 model操作用【Spring Boot Data JPA Starter】import

--- src/main/java/com/example/ecsitedeveloplearning/ec/shop/model/Product.java
+++ src/main/java/com/example/ecsitedeveloplearning/ec/shop/model/Product.java
@@ -1,10 +1,44 @@
 package com.example.ecsitedeveloplearning.ec.shop.model;
 
+import java.util.Date;
+
+import javax.persistence.*;
+
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "products")
 public class Product {
-	
 	// Databaseの Productsテーブルと連携するため Entity設定必要
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category categoryId;
+	
+	@Column(name = "price")
+	private Integer price;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "imagePath")
+	private String imagePath;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "created", nullable = false)
+	private Date created;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "updated", nullable = true)
+	private Date updated;
 
 }
--- src/main/java/com/example/ecsitedeveloplearning/ec/shop/web/ShopController.java
+++ src/main/java/com/example/ecsitedeveloplearning/ec/shop/web/ShopController.java
@@ -1,10 +1,16 @@
 package com.example.ecsitedeveloplearning.ec.shop.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.example.ecsitedeveloplearning.ec.shop.model.Product;
+import com.example.ecsitedeveloplearning.ec.shop.model.Category;
 import com.example.ecsitedeveloplearning.ec.shop.service.ShopService;
 
 @Controller
@@ -14,11 +20,19 @@ public class ShopController {
 	@Autowired
 	private ShopService shopService;
 	
+	@Value("${upload.image.path}")
+	private String uploadImagePath;
+	
 	// Top Page
 	@GetMapping("/top")
-	public String viewIndex() {
-		System.out.println("viewIndex");
-		return "shop/top";
+	public ModelAndView viewTop() {
+		ModelAndView mv = new ModelAndView("shop/top");
+		List<Product> products = shopService.findAllBycategory(1);
+		List<Category> categories = new shopService.findCategories();
+		mv.addObject("products", products);
+		mv.addObject("categories", categories);
+		
+		return mv;
 	}
 	
 
