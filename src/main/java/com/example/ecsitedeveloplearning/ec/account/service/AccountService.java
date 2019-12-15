7401010b3bb14c8acb2cdbfd415dec2610d66f5b
takumi_ishiyama <takumi_ishiyama@rosso-tokyo.co.jp>
2019-12-15 16:38:50 +0900

    Account, pom修正

--- src/main/java/com/example/ecsitedeveloplearning/ec/account/service/AccountService.java
+++ src/main/java/com/example/ecsitedeveloplearning/ec/account/service/AccountService.java
@@ -1,8 +1,19 @@
 package com.example.ecsitedeveloplearning.ec.account.service;
 
+import com.example.ecsitedeveloplearning.ec.account.model.Account;
+import com.example.ecsitedeveloplearning.ec.account.repository.AccountRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
-
+	@Autowired
+	private AccountRepository accountRepository;
+	
+	public List<Account> findAll() {
+		return accountRepository.findAll();
+	}
 }
