1a80b91edbdd5f5c9600679215e22bb348ca145e
takumi_ishiyama <takumi_ishiyama@rosso-tokyo.co.jp>
2019-12-15 16:39:24 +0900

    2019/12/15 Account, POM修正

---
+++ src/main/java/com/example/ecsitedeveloplearning/ec/account/model/UserType.java
@@ -0,0 +1,16 @@
+package com.example.ecsitedeveloplearning.ec.account.model;
+
+import lombok.Getter;
+
+public enum UserType {
+	
+	ADMIN("admin"),
+	USER("user");
+	
+	@Getter
+	private String value;
+	
+	private UserType(String value) {
+		this.value = value;
+	}
+}
